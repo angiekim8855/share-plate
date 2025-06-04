6e2d71b6634d672b0659a901dd729ef2ef58f0af
angiekim8855 <angiekim8855@gmail.com>
2025-06-04 22:49:07 +0900

    add: 기본 라우팅 구조

---
+++ src/navigation/authStackNavigator.tsx
@@ -0,0 +1,14 @@
+// src/navigation/AuthStackNavigator.tsx
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/Login";
+
+const Stack = createNativeStackNavigator();
+
+export default function AuthStackNavigator() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+    );
+}
