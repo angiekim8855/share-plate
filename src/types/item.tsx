99ef7ee1ce968bb8e070e22ff0d529ab9e30e3d6
angiekim8855 <angiekim8855@gmail.com>
2025-06-08 17:30:58 +0900

    add: mock data

---
+++ src/types/item.tsx
@@ -0,0 +1,10 @@
+export type Item = {
+    itemId: string;
+    itemName: string;
+    itemImg: string;
+    price: number;
+    finalPrice: number;
+    addDate: string;
+    ea: number;
+    orderNumberList: string[];
+};
---
+++ src/types/review.tsx
@@ -0,0 +1,8 @@
+export type Review = {
+    img: string;
+    userName: string;
+    orderNumber: string;
+    reviewId: string;
+    reviewDetail: string;
+    date: string;
+};
