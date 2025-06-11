0821ff33002d025f8f9c2fee8a505b030b28aa72
angiekim8855 <angiekim8855@gmail.com>
2025-06-11 16:06:51 +0900

    add: firebase config

---
+++ firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration -> Firebase가 만든 '내 프로젝트 정보'를 담은 접속키
+const firebaseConfig = {
+    apiKey: "AIzaSyC5OqXm3bmggNWAWFPMGUo_4R4XtNqpfyk",
+    authDomain: "share-plate-d9dd2.firebaseapp.com",
+    projectId: "share-plate-d9dd2",
+    storageBucket: "share-plate-d9dd2.firebasestorage.app",
+    messagingSenderId: "476378567931",
+    appId: "1:476378567931:web:d2c4d5e6fb789bd5a94c5e",
+};
+
+// Initialize Firebase -> Firebase JS SDK가 내 Firebase 콘솔의 프로젝트에 연결
+const app = initializeApp(firebaseConfig);
