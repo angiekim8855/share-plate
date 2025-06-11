6a2da1cb7aa8aaf30b73c4e90313350a2a98697f
angiekim8855 <angiekim8855@gmail.com>
2025-06-11 16:06:43 +0900

    fix: 대소문자 오류 해결

--- src/navigation/types.tsx
+++ src/navigation/types.tsx
@@ -1,6 +1,7 @@
 import { Restaurant } from "../types/store";
 
 export type RootStackParamList = {
+    Login: undefined;
     HomeMain: undefined;
     RestaurantDetail: { restaurant: Restaurant };
 }; 
