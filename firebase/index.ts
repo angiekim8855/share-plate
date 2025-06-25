a1ba79e7409bc09bbf9f658c911ae3b9416825de
angiekim8855 <angiekim8855@gmail.com>
2025-06-25 18:34:49 +0900

    add: 이미지 저장할 firebase 스토리지 연결

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration -> Firebase가 만든 '내 프로젝트 정보'를 담은 접속키
 const firebaseConfig = {
@@ -14,5 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase -> Firebase JS SDK가 내 Firebase 콘솔의 프로젝트에 연결
 const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
 export const db = getFirestore(app);
 // export const auth = getAuth(app);
