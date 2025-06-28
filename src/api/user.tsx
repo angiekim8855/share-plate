f0afaf3559652c3729040cfbf8cc5289eec1ebb2
angiekim8855 <angiekim8855@gmail.com>
2025-06-28 23:45:23 +0900

    add: 고객의 예약 정보 불러오기

---
+++ src/api/user.tsx
@@ -0,0 +1,19 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+
+export const fetchUserReservations = async (userId: string) => {
+    try {
+        const reservationRef = collection(db, "user", userId, "reservationList");
+        const snapshot = await getDocs(reservationRef);
+
+        const reservationList = snapshot.docs.map((doc) => ({
+            reservationId: doc.id,
+            ...doc.data(),
+        }));
+
+        return reservationList;
+    } catch (error) {
+        console.error("예약 목록 불러오기 실패:", error);
+        return [];
+    }
+};
