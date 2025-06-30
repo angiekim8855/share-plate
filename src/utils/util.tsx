5def466780b040679769b98b37ba0410b219e7d5
angiekim8855 <angiekim8855@gmail.com>
2025-06-30 18:39:19 +0900

    fix: 이미지 url이 storage에 존재하는지 체크

--- src/utils/util.tsx
+++ src/utils/util.tsx
@@ -1,3 +1,6 @@
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from "../../firebase";
+
 export const generateOrderNumber = () => {
     const date = new Date();
     const yyyyMMdd = date.toISOString().slice(0, 10).replace(/-/g, ""); // 20250604
@@ -8,3 +11,17 @@ export const generateOrderNumber = () => {
 export const formatPrice = (value: number) => value.toLocaleString();
 
 export const randomId = () => Math.random().toString(36).substr(2, 9);
+
+export const checkImageExists = async (url: string) => {
+    try {
+        // 스토리지 경로 추출
+        const imagePath = decodeURIComponent(new URL(url).pathname.replace("/o/", "").split("?")[0]);
+
+        const imageRef = ref(storage, imagePath);
+        await getDownloadURL(imageRef);
+        return true;
+    } catch (error) {
+        console.warn("이미지 존재 확인 실패:", error);
+        return false;
+    }
+};
