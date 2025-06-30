5d794412eab4c2f2633ca508c67952cec34b0e51
angiekim8855 <angiekim8855@gmail.com>
2025-06-30 23:43:06 +0900

    add: 가게 카테고리 constant

---
+++ src/constant.tsx
@@ -0,0 +1,12 @@
+export const StoreCategory = {
+    korean: "한식",
+    western: "양식",
+    street: "분식",
+    chinese: "중식",
+    japanese: "일식",
+    dessert: "디저트",
+    bakery: "베이커리",
+    fruit: "과일",
+    salad: "샐러드",
+    etc: "기타",
+};
