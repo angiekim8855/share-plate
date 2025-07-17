a220ea4a6d6a846afa8e30e890faa743da2a2151
angiekim8855 <angiekim8855@gmail.com>
2025-07-17 22:43:00 +0900

    add: 가게 카테고리 상수 설정

--- src/constant.tsx
+++ src/constant.tsx
@@ -1,4 +1,4 @@
-export const StoreCategory = {
+export const StoreCategory: { [key: string]: string } = {
     korean: "한식",
     western: "양식",
     street: "분식",
@@ -9,4 +9,17 @@ export const StoreCategory = {
     fruit: "과일",
     salad: "샐러드",
     etc: "기타",
+};
+
+export const categoryColors: { [key: string]: string } = {
+    korean: "#FF5C5C", // 강렬한 레드
+    western: "#FF9900", // 주황
+    street: "#FFCC00", // 노랑
+    chinese: "#FF6666", // 밝은 빨강
+    japanese: "#FF3399", // 핑크
+    dessert: "#9966FF", // 보라
+    bakery: "#FF9966", // 살구
+    fruit: "#33CC33", // 선명한 그린
+    salad: "#00CC99", // 청록
+    etc: "#999999", // 중립 회색
 };
