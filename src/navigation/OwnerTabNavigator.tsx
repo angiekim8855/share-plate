0bbf0a26a3f5e76401949cb8cb983edb3c958e29
angiekim8855 <angiekim8855@gmail.com>
2025-07-24 23:14:29 +0900

    fix: 네비게이션 tab bar 스타일 수정

--- src/navigation/OwnerTabNavigator.tsx
+++ src/navigation/OwnerTabNavigator.tsx
@@ -8,7 +8,18 @@ const Tab = createBottomTabNavigator();
 
 export default function OwnerTabNavigator() {
     return (
-        <Tab.Navigator screenOptions={{ headerShown: true }}>
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: true,
+                tabBarIcon: () => null,
+                tabBarIconStyle: { display: "none" },
+                tabBarLabelStyle: {
+                    fontSize: 16,
+                    fontWeight: "600",
+                    paddingTop: 10,
+                },
+            }}
+        >
             <Tab.Screen name="ReservationList" component={ReservationList} options={{ title: "예약관리" }} />
             <Tab.Screen name="ItemManage" component={ItemManage} options={{ title: "메뉴 관리" }} />
             <Tab.Screen name="OwnerMyPage" component={OwnerMyPage} options={{ title: "마이페이지" }} />
