73023f9df6830a764abb78678f8d6d6e5da2d372
angiekim8855 <angiekim8855@gmail.com>
2025-08-15 00:14:21 +0900

    fix: 인증키 보안

--- firebase/index.ts
+++ firebase/index.ts
@@ -7,7 +7,6 @@ import Constants from "expo-constants";
 const { firebaseApiKey, firebaseAuthDomain, firebaseProjectId, firebaseStorageBucket, firebaseMessagingSenderId, firebaseAppId } =
     Constants.expoConfig?.extra || {};
 
-// Your web app's Firebase configuration -> Firebase가 만든 '내 프로젝트 정보'를 담은 접속키
 const firebaseConfig = {
     apiKey: firebaseApiKey,
     authDomain: firebaseAuthDomain,
@@ -17,7 +16,6 @@ const firebaseConfig = {
     appId: firebaseAppId,
 };
 
-// Initialize Firebase -> Firebase JS SDK가 내 Firebase 콘솔의 프로젝트에 연결
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
